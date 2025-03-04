2ce2c78ef868f3ae68d7af845e2643de39e2157c
mka <mka@A34PC12>
2025-03-04 14:56:13 +0200

    /*Створити програму для обліку книг, довідок про про наявність книг, журналів, газет. Реалізувати наступні сервіси:  Заповнення бази даних  Перегляд даних про всі джерела  Доповнення бази даних записом джерела  Видалення джерела із бази даних  Упорядкування по полях: тип інформаційного джерела (книга, журнал, газета) і назва  Пошук: наявність заданої книги (відомі автор і назва), наявність заданого журналу  Вибірка: книги автора ХХ; книги певної категорії (фантастика, детектив тощо), журнали за певний рік (відомі рік і назва журналу)  Обчислення: кількість книг деякої категорії  Корекція: видалення зведень про газети за певний рік  Табличний звіт: список боржників книг певного автора

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("welcome to the library");
+Console.WriteLine("-----menu----");
+Console.WriteLine("database");
+Console.WriteLine("Viewing data about all sources");
+Console.WriteLine("delete all sources");
+Console.WriteLine("source record to the database");
+Console.WriteLine("Search");
+Console.WriteLine("Selection");
+Console.WriteLine("list");
+
+/*Створити програму для обліку книг, довідок про про наявність книг, журналів, газет. Реалізувати наступні сервіси:
+ Заповнення бази даних 
+ Перегляд даних про всі джерела
+ Доповнення бази даних записом джерела
+ Видалення джерела із бази даних
+ Упорядкування по полях: тип інформаційного джерела (книга, журнал, газета) і назва
+ Пошук: наявність заданої книги (відомі автор і назва), наявність заданого журналу
+ Вибірка: книги автора ХХ; книги певної категорії (фантастика, детектив тощо), журнали за певний рік (відомі рік і назва журналу)
+ Обчислення: кількість книг деякої категорії
+ Корекція: видалення зведень про газети за певний рік
+ Табличний звіт: список боржників книг певного автора*/
